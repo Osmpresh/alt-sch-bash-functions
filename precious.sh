4b60ccda34b0d6247c43e6b22196a0267b0e71ba
Osm <osmpresh2000@gmail.com>
2025-01-03 13:05:42 +0100

    Add precious.sh

---
+++ precious.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+who_am_i(){
+	echo "Hello, My name is Osimen Precious Ebanehita."
+	echo "I Love Tech!"
+	echo "Over a Year Now."
+	echo "To become a FullStack Engineer."
+	echo "To bring the gospel to my World through Tech."
+	}	
+
+who_am_i
+
+
+
